4f87a4d535f2eb8ebcc3b85982dde346d3f5b2c6
divya-prav <divya.praveen.5215@gmail.com>
2023-12-21 12:38:43 -0600

    Done:Inorder function

--- block-49/inOrder.js
+++ block-49/inOrder.js
@@ -1,3 +1,28 @@
-function inOrder(){
-    
+class TreeNode {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
+let inOrderTraversal = [];
+function createBinaryTree() {
+  let tree = new TreeNode(1);
+  tree.left = new TreeNode(2);
+  tree.right = new TreeNode(3);
+  tree.left.left = new TreeNode(4);
+  tree.left.right = new TreeNode(5);
+  return tree;
+}
+
+function inOrder(tree) {
+  if (tree !== null) {
+    inOrder(tree.left);
+    inOrderTraversal.push(tree.data);
+    inOrder(tree.right);
+  }
+}
+
+let binaryTree = createBinaryTree();
+inOrder(binaryTree);
+console.log(inOrderTraversal);
